1c05e2f38586a620db4f3d1a6865473d3ef3fc0a
AdeptDivinity <kevinreid3light@gmail.com>
2019-11-12 07:41:26 -0500

    git init the current work build

---
+++ src/index.js
@@ -0,0 +1,22 @@
+let galleryCont = document.getElementsByClassName('gallery-cont')[0]
+let homeBack = document.getElementsByClassName('background-cont')[0]
+
+function homeSlideShow(){
+    let index = 0;
+    let showArray = [0,1,2,3]
+    let changeSlide = ()=>{
+        homeBack.style.background = `url('./src/images/restaurant${showArray[index]}.jpg') center -40px no-repeat `;
+        homeBack.style['background-size'] = '100%'
+    }
+    setInterval(()=>{
+        index += 1
+        if(index === showArray.length ){index = 0}
+        changeSlide()
+    },10000)
+    
+    changeSlide()
+    
+}
+
+
+homeSlideShow()
